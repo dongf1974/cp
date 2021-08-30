17cc98e976c92cc10384b4f2be1bcfb071d7d7a6
dongf1974 <dongf@live.com>
2021-08-31 01:59:37 +0800

    P1, CCC2021J1, by Eric, reviewed by Dong, Fang

---
+++ eric.P1.CCC2021J1.20210812.review.20210818.py
@@ -0,0 +1,42 @@
+#
+# Author: Eric Zhang
+# Reviewer: Dong, Fang
+#
+
+#
+# CCC 2021 Problem J1: Boiling Water
+# http://cemc.uwaterloo.ca/contests/computing/2021/index.html
+#
+
+boil=int(input())
+pressure=5*boil-400
+print(pressure)
+if pressure > 100:
+    print("-1")
+if pressure < 100:
+    print("1")
+if pressure == 100:
+    print(0)
+
+#
+# problem: logic error
+#             wrong variable
+# problem: usage of 'if...elif...else'
+# problem: usage of print()
+#             print("1") VS print(1)
+#             print("-1") VS print(-1)
+#
+
+boil=int(input())
+pressure=5*boil-400
+print(pressure)
+if boil > 100:
+    print(-1)
+elif boil < 100:
+    print(1)
+else:
+    print(0)
+
+#
+# END
+#
