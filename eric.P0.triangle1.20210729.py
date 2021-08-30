12cfe79a2e74a38aa73a675d408389fed4e3f512
dongf1974 <dongf@live.com>
2021-08-31 01:35:02 +0800

    P0, Triangle 1, by Michael, Eric

---
+++ eric.P0.triangle1.20210729.py
@@ -0,0 +1,21 @@
+#
+# Author: Eric Zhang
+#
+
+#
+# input:
+#     <line count>
+#
+# output:
+#     *
+#     **
+#     ...
+#     **...*
+#
+
+x=int(input("Input A number:"))
+y=1
+for i in range(x)
+	for j in range(y)
+		print("*", end='')
+	y=y+1
---
+++ michael.P0.triangle1.20210729.py
@@ -0,0 +1,20 @@
+#
+# Author: Micheal Wu
+#
+
+#
+# input:
+#     <line count>
+#
+# output:
+#     *
+#     **
+#     ...
+#     **...*
+#
+
+n=int(input())
+for i in range(n):
+	print()
+	for j in range(i+1):
+		print("*", end = "")
